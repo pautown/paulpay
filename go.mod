d39c7b4a550e21cf3238335c8feb99abc7fee97d
Paul <50934077+pautown@users.noreply.github.com>
2023-02-28 00:05:33 -0500

    Add routing and user db and logic

--- go.mod
+++ go.mod
@@ -7,9 +7,12 @@ require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/portto/solana-go-sdk v1.23.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -22,18 +22,14 @@ import (
 	"context"
 	"crypto/ed25519"
 
-	//"encoding/json"
-	"bufio"
 	"encoding/base64"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"html"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/smtp"
 	"net/url"
 	"os"
 	"reflect"
@@ -50,6 +46,13 @@ import (
 	"github.com/portto/solana-go-sdk/rpc"
 	"github.com/portto/solana-go-sdk/types"
 	qrcode "github.com/skip2/go-qrcode"
+
+
+
+	"crypto/rand"
+	"github.com/google/uuid"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var USDMinimum float64 = 5
@@ -73,16 +76,47 @@ var checked string = ""
 
 var indexTemplate *template.Template
 var payTemplate *template.Template
-var checkXMRTemplate *template.Template
-var checkSOLTemplate *template.Template
 var alertTemplate *template.Template
 var viewTemplate *template.Template
+
+var loginTemplate *template.Template
+var incorrectLoginTemplate *template.Template
+var userTemplate *template.Template
+var logoutTemplate *template.Template
+var incorrectPasswordTemplate *template.Template
+
+
 
 // Mainnet
 //var c = client.NewClient(rpc.MainnetRPCEndpoint)
 
 // Devnet
 var c = client.NewClient(rpc.DevnetRPCEndpoint)
+
+
+
+type User struct {
+	UserID               int
+	Username             string
+	HashedPassword       []byte
+	Timezone             string
+	EthAddress           string
+	SolAddress           string
+	HexcoinAddress       string
+	XMRWalletPassword    string
+	MinDono              int
+	MinMediaDono         int
+	MediaEnabled         bool
+	CreationDatetime     string
+	ModificationDatetime string
+}
+
+type UserPageData struct {
+	ErrorMessage string
+}
+
+var db *sql.DB
+var userSessions = make(map[string]int)
 
 func MakeRequest(URL string) string {
 	client := &http.Client{}
@@ -197,34 +231,56 @@ type MoneroPrice struct {
 
 
 func main() {
-	jsonFile, err := os.Open("config.json")
+	log.Println("Starting server")
+	var err error
+	db, err = sql.Open("sqlite3", "./users.db")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println("reading config.json")
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}(jsonFile)
-	byteValue, _ := io.ReadAll(jsonFile)
-	var conf configJson
-	err = json.Unmarshal(byteValue, &conf)
+	defer db.Close()
+
+	// create users table if not exists
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+		UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Username TEXT UNIQUE,
+		HashedPassword BLOB,
+		Timezone TEXT,
+		EthAddress TEXT,
+		SolAddress TEXT,
+		HexcoinAddress TEXT,
+		XMRWalletPassword TEXT,
+		MinDono INTEGER,
+		MinMediaDono INTEGER,
+		MediaEnabled BOOLEAN,
+		CreationDatetime TEXT,
+		ModificationDatetime TEXT
+	)`)
 	if err != nil {
-		panic(err) // Fatal error, stop program
+		log.Fatal(err)
 	}
 
-	ScamThreshold = conf.MinimumDonation
-	MessageMaxChar = conf.MaxMessageChars
-	NameMaxChar = conf.MaxNameChars
-	rpcURL = conf.RPCWalletURL
-	username = conf.WebViewUsername
-	password = conf.WebViewPassword
-	AlertWidgetRefreshInterval = conf.OBSWidgetRefresh
-	if conf.Checked == true {
-		checked = " checked"
+	// create admin user if not exists
+	adminUser := User{
+		Username:          "admin",
+		XMRWalletPassword: "",
+		MinDono:           0,
+		MinMediaDono:      0,
+		MediaEnabled:      true,
 	}
+	adminHashedPassword, err := bcrypt.GenerateFromPassword([]byte("hunter123"), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	adminUser.HashedPassword = adminHashedPassword
+
+	err = createUser(adminUser)
+	if err != nil {
+		log.Println(err)
+	}
+
+	
+
+
 
 	// create a RPC client
 	fmt.Println(reflect.TypeOf(c))
@@ -255,31 +311,321 @@ func main() {
 	http.HandleFunc("/alert", alertHandler)
 	http.HandleFunc("/view", viewHandler)
 
-	// Create files if they don't exist
-	_, err = os.OpenFile("log/paid.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
 
-	_, err = os.OpenFile("log/alertqueue.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
+	// serve login and user interface pages
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/incorrect_login", incorrectLoginHandler)
+	http.HandleFunc("/user", userHandler)
+	http.HandleFunc("/logout", logoutHandler)
+	http.HandleFunc("/changepassword", changePasswordHandler)
 
-	_, err = os.OpenFile("log/superchats.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
 
 	indexTemplate, _ = template.ParseFiles("web/index.html")
 	payTemplate, _ = template.ParseFiles("web/pay.html")
 	alertTemplate, _ = template.ParseFiles("web/alert.html")
 	viewTemplate, _ = template.ParseFiles("web/view.html")
+
+
+
+	loginTemplate, _ = template.ParseFiles("web/login.html")
+	incorrectLoginTemplate, _ = template.ParseFiles("web/incorrect_login.html")
+	userTemplate, _ = template.ParseFiles("web/user.html")
+	logoutTemplate, _ = template.ParseFiles("web/logout.html")
+	incorrectPasswordTemplate, _ = template.ParseFiles("web/password_change_failed.html")
+
+
 	err = http.ListenAndServe(":8900", nil)
 	if err != nil {
 		panic(err)
 	}
 }
+
+// create a new user
+func createUser(user User) error {
+	statement := `
+		INSERT INTO users (Username, HashedPassword, Timezone, EthAddress, SolAddress, HexcoinAddress,
+			XMRWalletPassword, MinDono, MinMediaDono, MediaEnabled, CreationDatetime, ModificationDatetime)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, datetime('now'), datetime('now'))
+	`
+	_, err := db.Exec(statement, user.Username, user.HashedPassword, user.Timezone, user.EthAddress,
+		user.SolAddress, user.HexcoinAddress, user.XMRWalletPassword, user.MinDono, user.MinMediaDono,
+		user.MediaEnabled)
+	return err
+}
+
+// update an existing user
+func updateUser(user User) error {
+	statement := `
+		UPDATE users
+		SET Username=?, HashedPassword=?, Timezone=?, EthAddress=?, SolAddress=?, HexcoinAddress=?,
+			XMRWalletPassword=?, MinDono=?, MinMediaDono=?, MediaEnabled=?, ModificationDatetime=datetime('now')
+		WHERE UserID=?
+	`
+	_, err := db.Exec(statement, user.Username, user.HashedPassword, user.Timezone, user.EthAddress,
+		user.SolAddress, user.HexcoinAddress, user.XMRWalletPassword, user.MinDono, user.MinMediaDono,
+		user.MediaEnabled, user.UserID)
+	return err
+}
+
+// get a user by their username
+func getUserByUsername(username string) (User, error) {
+	var user User
+	row := db.QueryRow("SELECT * FROM users WHERE Username=?", username)
+	err := row.Scan(&user.UserID, &user.Username, &user.HashedPassword, &user.Timezone, &user.EthAddress,
+		&user.SolAddress, &user.HexcoinAddress, &user.XMRWalletPassword, &user.MinDono, &user.MinMediaDono,
+		&user.MediaEnabled, &user.CreationDatetime, &user.ModificationDatetime)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+// get a user by their session token
+func getUserBySession(sessionToken string) (User, error) {
+	userID, ok := userSessions[sessionToken]
+	if !ok {
+		return User{}, fmt.Errorf("session token not found")
+	}
+	var user User
+	row := db.QueryRow("SELECT * FROM users WHERE UserID=?", userID)
+	err := row.Scan(&user.UserID, &user.Username, &user.HashedPassword, &user.Timezone, &user.EthAddress,
+		&user.SolAddress, &user.HexcoinAddress, &user.XMRWalletPassword, &user.MinDono, &user.MinMediaDono,
+		&user.MediaEnabled, &user.CreationDatetime, &user.ModificationDatetime)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+// verify that the entered password matches the stored hashed password for a user
+func verifyPassword(user User, password string) bool {
+	err := bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
+	return err == nil
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		user, err := getUserByUsername(username)
+
+		if err != nil {
+			if err.Error() == "sql: no rows in result set" { // can't find username in DB
+				http.Redirect(w, r, "/incorrect_login", http.StatusFound)
+				return
+			}
+
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		if user.UserID == 0 || !verifyPassword(user, password) {
+			http.Redirect(w, r, "/incorrect_login", http.StatusFound)
+			return
+		}
+
+		sessionToken, err := createSession(user.UserID)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session",
+			Value:    sessionToken,
+			HttpOnly: true,
+			Path:     "/",
+			SameSite: http.SameSiteStrictMode,
+			Secure:   true,
+		})
+		http.Redirect(w, r, "/user", http.StatusFound)
+		return
+	}
+	tmpl := template.Must(template.ParseFiles("web/login.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+// handle requests to modify user data
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	user, err := getUserBySession(cookie.Value)
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if r.Method == "POST" {
+		user.Username = r.FormValue("username")
+		user.Timezone = r.FormValue("timezone")
+		user.EthAddress = r.FormValue("ethaddress")
+		user.SolAddress = r.FormValue("soladdress")
+		user.HexcoinAddress = r.FormValue("hexcoinaddress")
+		user.XMRWalletPassword = r.FormValue("xmrwalletpassword")
+		minDono, _ := strconv.Atoi(r.FormValue("mindono"))
+		user.MinDono = minDono
+		minMediaDono, _ := strconv.Atoi(r.FormValue("minmediadono"))
+		user.MinMediaDono = minMediaDono
+		mediaEnabled := r.FormValue("mediaenabled") == "on"
+		user.MediaEnabled = mediaEnabled
+
+		err := updateUser(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/user", http.StatusSeeOther)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("web/user.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, user)
+
+}
+
+func generateSessionToken(user User) string {
+	// generate a random session token
+	b := make([]byte, 32)
+	rand.Read(b)
+	sessionToken := base64.URLEncoding.EncodeToString(b)
+	// save the session token in a map
+	userSessions[sessionToken] = user.UserID
+	return sessionToken
+}
+
+func changePasswordHandler(w http.ResponseWriter, r *http.Request) {
+	// retrieve user from session
+	sessionToken, err := r.Cookie("session")
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	user, err := getUserBySession(sessionToken.Value)
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// initialize user page data struct
+	data := UserPageData{}
+
+	// process form submission
+	if r.Method == "POST" {
+		// check current password
+		if !verifyPassword(user, r.FormValue("current_password")) {
+			// set user page data values
+			data.ErrorMessage = "Current password entered was incorrect"
+			// render password change failed form
+			tmpl, err := template.ParseFiles("web/password_change_failed.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			tmpl.Execute(w, data)
+			return
+		} else {
+			// hash new password
+			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(r.FormValue("new_password")), bcrypt.DefaultCost)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// update user password in database
+			user.HashedPassword = hashedPassword
+			err = updateUser(user)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// redirect to user page
+			http.Redirect(w, r, "/user", http.StatusSeeOther)
+			return
+		}
+	}
+
+
+	// render change password form
+	tmpl, err := template.ParseFiles("web/user.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, data)
+}
+
+func renderChangePasswordForm(w http.ResponseWriter, data UserPageData) {
+	tmpl, err := template.ParseFiles("web/user.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, data)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// invalidate session token
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session",
+		Value:    "",
+		MaxAge:   -1,
+		HttpOnly: true,
+	})
+
+	// redirect user to login page
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func incorrectLoginHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/incorrect_login.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func createSession(userID int) (string, error) {
+	sessionToken := uuid.New().String()
+	userSessions[sessionToken] = userID
+	return sessionToken, nil
+}
+
+func validateSession(r *http.Request) (int, error) {
+	sessionToken, err := r.Cookie("session")
+	if err != nil {
+		return 0, fmt.Errorf("no session token found")
+	}
+	userID, ok := userSessions[sessionToken.Value]
+	if !ok {
+		return 0, fmt.Errorf("invalid session token")
+	}
+	return userID, nil
+}
+
+
+
+
+
 
 func createWalletSolana(dName string, dString string, dAmount float64, dAnon bool) AddressSolana {
 	//create a new wallet using
