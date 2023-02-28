cfecea4bf8a3fdc8be03c7ae35bcfffac06d0adc
Paul <50934077+pautown@users.noreply.github.com>
2023-02-27 23:34:57 -0500

    Add tempmain.go also remove cruft
    Remove mail features
    Begin movement from from CSV to DB

--- main.go
+++ main.go
@@ -43,6 +43,9 @@ import (
 	"time"
 	"unicode/utf8"
 
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+
 	"github.com/portto/solana-go-sdk/client"
 	"github.com/portto/solana-go-sdk/rpc"
 	"github.com/portto/solana-go-sdk/types"
@@ -66,13 +69,7 @@ var addressSliceSolana []AddressSolana
 var password string = "adminadmin"
 var checked string = ""
 
-// Email settings
-var enableEmail bool = false
-var smtpHost string = "smtp.purelymail.com"
-var smtpPort string = "587"
-var smtpUser string = "example@purelymail.com"
-var smtpPass string = "[y7EQ(xgTW_~{CUpPhO6(#"
-var sendTo = []string{"example@purelymail.com"} // Comma separated recipient list
+
 
 var indexTemplate *template.Template
 var payTemplate *template.Template
@@ -80,7 +77,6 @@ var checkXMRTemplate *template.Template
 var checkSOLTemplate *template.Template
 var alertTemplate *template.Template
 var viewTemplate *template.Template
-var topWidgetTemplate *template.Template
 
 // Mainnet
 //var c = client.NewClient(rpc.MainnetRPCEndpoint)
@@ -124,12 +120,6 @@ type configJson struct {
 	WebViewPassword  string   `json:"WebViewPassword"`
 	OBSWidgetRefresh string   `json:"OBSWidgetRefresh"`
 	Checked          bool     `json:"ShowAmountCheckedByDefault"`
-	EnableEmail      bool     `json:"EnableEmail"`
-	SMTPServer       string   `json:"SMTPServer"`
-	SMTPPort         string   `json:"SMTPPort"`
-	SMTPUser         string   `json:"SMTPUser"`
-	SMTPPass         string   `json:"SMTPPass"`
-	SendToEmail      []string `json:"SendToEmail"`
 }
 
 type checkPage struct {
@@ -196,19 +186,7 @@ type AddressSolana struct {
 	DonoAnon   bool
 }
 
-type getAddress struct {
-	ID      string `json:"id"`
-	Jsonrpc string `json:"jsonrpc"`
-	Result  struct {
-		Address   string `json:"address"`
-		Addresses []struct {
-			Address      string `json:"address"`
-			AddressIndex int    `json:"address_index"`
-			Label        string `json:"label"`
-			Used         bool   `json:"used"`
-		} `json:"addresses"`
-	} `json:"result"`
-}
+
 
 type MoneroPrice struct {
 	Monero struct {
@@ -216,61 +194,7 @@ type MoneroPrice struct {
 	} `json:"monero"`
 }
 
-type GetTransfersResponse struct {
-	ID      string `json:"id"`
-	Jsonrpc string `json:"jsonrpc"`
-	Result  struct {
-		In []struct {
-			Address         string  `json:"address"`
-			Amount          int64   `json:"amount"`
-			Amounts         []int64 `json:"amounts"`
-			Confirmations   int     `json:"confirmations"`
-			DoubleSpendSeen bool    `json:"double_spend_seen"`
-			Fee             int     `json:"fee"`
-			Height          int     `json:"height"`
-			Locked          bool    `json:"locked"`
-			Note            string  `json:"note"`
-			PaymentID       string  `json:"payment_id"`
-			SubaddrIndex    struct {
-				Major int `json:"major"`
-				Minor int `json:"minor"`
-			} `json:"subaddr_index"`
-			SubaddrIndices []struct {
-				Major int `json:"major"`
-				Minor int `json:"minor"`
-			} `json:"subaddr_indices"`
-			SuggestedConfirmationsThreshold int    `json:"suggested_confirmations_threshold"`
-			Timestamp                       int    `json:"timestamp"`
-			Txid                            string `json:"txid"`
-			Type                            string `json:"type"`
-			UnlockTime                      int    `json:"unlock_time"`
-		} `json:"in"`
-		Pool []struct {
-			Address         string  `json:"address"`
-			Amount          int64   `json:"amount"`
-			Amounts         []int64 `json:"amounts"`
-			DoubleSpendSeen bool    `json:"double_spend_seen"`
-			Fee             int     `json:"fee"`
-			Height          int     `json:"height"`
-			Locked          bool    `json:"locked"`
-			Note            string  `json:"note"`
-			PaymentID       string  `json:"payment_id"`
-			SubaddrIndex    struct {
-				Major int `json:"major"`
-				Minor int `json:"minor"`
-			} `json:"subaddr_index"`
-			SubaddrIndices []struct {
-				Major int `json:"major"`
-				Minor int `json:"minor"`
-			} `json:"subaddr_indices"`
-			SuggestedConfirmationsThreshold int    `json:"suggested_confirmations_threshold"`
-			Timestamp                       int    `json:"timestamp"`
-			Txid                            string `json:"txid"`
-			Type                            string `json:"type"`
-			UnlockTime                      int    `json:"unlock_time"`
-		} `json:"pool"`
-	} `json:"result"`
-}
+
 
 func main() {
 	jsonFile, err := os.Open("config.json")
@@ -298,12 +222,6 @@ func main() {
 	username = conf.WebViewUsername
 	password = conf.WebViewPassword
 	AlertWidgetRefreshInterval = conf.OBSWidgetRefresh
-	enableEmail = conf.EnableEmail
-	smtpHost = conf.SMTPServer
-	smtpPort = conf.SMTPPort
-	smtpUser = conf.SMTPUser
-	smtpPass = conf.SMTPPass
-	sendTo = conf.SendToEmail
 	if conf.Checked == true {
 		checked = " checked"
 	}
@@ -319,7 +237,6 @@ func main() {
 
 	fmt.Println("version", response.SolanaCore)
 
-	fmt.Println(fmt.Sprintf("email notifications enabled?: %t", enableEmail))
 	fmt.Println(fmt.Sprintf("OBS Alert path: /alert?auth=%s", password))
 
 	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
@@ -335,11 +252,8 @@ func main() {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/pay", paymentHandler)
-	http.HandleFunc("/checkxmr", checkHandlerMonero)
-	http.HandleFunc("/checksol", checkHandlerSolana)
 	http.HandleFunc("/alert", alertHandler)
 	http.HandleFunc("/view", viewHandler)
-	http.HandleFunc("/top", topwidgetHandler)
 
 	// Create files if they don't exist
 	_, err = os.OpenFile("log/paid.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -359,11 +273,8 @@ func main() {
 
 	indexTemplate, _ = template.ParseFiles("web/index.html")
 	payTemplate, _ = template.ParseFiles("web/pay.html")
-	checkXMRTemplate, _ = template.ParseFiles("web/checkXMR.html")
-	checkSOLTemplate, _ = template.ParseFiles("web/checkSOL.html")
 	alertTemplate, _ = template.ParseFiles("web/alert.html")
 	viewTemplate, _ = template.ParseFiles("web/view.html")
-	topWidgetTemplate, _ = template.ParseFiles("web/top.html")
 	err = http.ListenAndServe(":8900", nil)
 	if err != nil {
 		panic(err)
@@ -398,20 +309,7 @@ func addToAddressSliceSolana(a AddressSolana) {
 	fmt.Println(len(addressSliceSolana))
 }
 
-func mail(name string, amount string, message string) {
-	body := []byte(fmt.Sprintf("From: %s\n"+
-		"Subject: %s sent %s XMR\nDate: %s\n\n"+
-		"%s", smtpUser, name, amount, fmt.Sprint(time.Now().Format(time.RFC1123Z)), message))
 
-	auth := smtp.PlainAuth("", smtpUser, smtpPass, smtpHost)
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, smtpUser, sendTo, body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("email sent")
-}
 
 func condenseSpaces(s string) string {
 	return strings.Join(strings.Fields(s), " ")
@@ -480,82 +378,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func checkHandlerSolana(w http.ResponseWriter, r *http.Request) {
-
-	var solanaAddress = r.URL.Query().Get("id")
-	var amountToCheck, _ = strconv.ParseFloat(r.URL.Query().Get("amount"), 64)
-	var anonDono = false
-
-	if html.EscapeString(r.URL.Query().Get("showAmount")) == "false" {
-		anonDono = true
-	}
-
-	rpcURL := "https://api.mainnet-beta.solana.com"
-	payload := strings.NewReader(`{"jsonrpc":"2.0","id":1,"method":"getBalance","params":["` + solanaAddress + `"]}`)
-	req, err := http.NewRequest("POST", rpcURL, payload)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer res.Body.Close()
-
-	var resp getBalanceResponse
-	err = json.NewDecoder(res.Body).Decode(&resp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var c checkPage
-	c.Meta = `<meta http-equiv="Refresh" content="3">`
-	c.Addy = solanaAddress
-	c.PayID = r.FormValue("id")
-	c.Name = truncateStrings(r.FormValue("name"), NameMaxChar)
-	c.Msg = truncateStrings(r.FormValue("msg"), MessageMaxChar)
-	c.Media = r.FormValue("media")
-	c.Received = float64(resp.Result.Value)
-	c.Receipt = "Waiting for payment..."
-
-	if c.Received >= amountToCheck {
-		c.Meta = ""
-		c.Received = float64(resp.Result.Value) / 1000000000
-
-		if c.Received < ScamThreshold { // right now minimum threshold is in monero
-			c.Receipt = "<b style='color:red'>Scammed! " + fmt.Sprint(c.Received) + " is below minimum</b>"
-		} else { // If dono is above the threshold
-
-			c.Receipt = "<b>" + fmt.Sprint(c.Received) + " SOL Received! Superchat sent</b>"
-			if c.Received < MediaMin || c.Msg == "" {
-				c.Msg = "⠀"
-			}
-			// Add the payment to the log
-			// ...
-			//var moneroBool = false
-			//if r.FormValue("mon") == "true" {
-			//	moneroBool = true
-			//}
-			addPaymentToLog(c.Name, c.Msg, c.Media, "SOL", c.Received, anonDono)
-
-		}
-	} else {
-		fmt.Println("Checking solana address:" + c.Addy)
-		fmt.Println("How much is needed:" + fmt.Sprintf("%.5f", amountToCheck))
-		fmt.Println("How much has been sent:" + fmt.Sprintf("%.5f", c.Received))
-	}
-
-	// Render the HTML response
-	err = checkSOLTemplate.Execute(w, c)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
 
 func addPaymentToLog(name, message, media, currency string, amount float64, anon bool) {
 	//var datetime = getCurrentDateTime()
@@ -573,273 +395,8 @@ func getCurrentDateTime() string {
 	return now.Format("2006-01-02 15:04:05")
 }
 
-func checkHandlerMonero(w http.ResponseWriter, r *http.Request) {
 
-	payload := strings.NewReader(`{"jsonrpc":"2.0","id":"0","method":"get_address"}`)
-	req, _ := http.NewRequest("POST", rpcURL, payload)
-	req.Header.Set("Content-Type", "application/json")
-	res, _ := http.DefaultClient.Do(req)
-	resp := &getAddress{}
 
-	if err := json.NewDecoder(res.Body).Decode(resp); err != nil {
-		fmt.Println(err.Error())
-	}
-
-	var c checkPage
-	c.Meta = `<meta http-equiv="Refresh" content="3">`
-	c.Addy = resp.Result.Address
-	c.PayID = r.FormValue("id")
-	c.Name = truncateStrings(r.FormValue("name"), NameMaxChar)
-	c.Msg = truncateStrings(r.FormValue("msg"), MessageMaxChar)
-	c.Media = r.FormValue("media")
-	c.Receipt = "Waiting for payment..."
-
-	payload2 := strings.NewReader(`{"jsonrpc":"2.0","id":"0","method":"get_transfers","params":{"in":true,"pool":true,"account_index":0}}`)
-	req2, _ := http.NewRequest("POST", "http://127.0.0.1:28088/json_rpc", payload2)
-
-	req2.Header.Set("Content-Type", "application/json")
-	res2, _ := http.DefaultClient.Do(req2)
-	resp2 := &GetTransfersResponse{}
-
-	if err := json.NewDecoder(res2.Body).Decode(resp2); err != nil {
-		fmt.Println(err.Error())
-	}
-
-	for _, tx := range resp2.Result.In {
-		if tx.PaymentID == c.PayID {
-			var logged = false
-			file, err := os.Open("log/paid.log")
-			if err != nil {
-				log.Fatalf("failed to open ")
-
-			}
-
-			scanner := bufio.NewScanner(file)
-			scanner.Split(bufio.ScanLines)
-			var text []string
-
-			for scanner.Scan() {
-				text = append(text, scanner.Text())
-			}
-
-			err = file.Close()
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			for _, eachLn := range text {
-				if eachLn == tx.PaymentID {
-					logged = true
-				}
-			}
-
-			if !logged {
-
-				f, err := os.OpenFile("log/paid.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-				if err != nil {
-					log.Println(err)
-				}
-				defer func(f *os.File) {
-					err := f.Close()
-					if err != nil {
-						fmt.Println(err)
-					}
-				}(f)
-				if _, err := f.WriteString(tx.PaymentID + "\n"); err != nil {
-					log.Println(err)
-				}
-
-				c.Meta = ""
-				c.Received = float64(tx.Amount) / 1000000000000
-				if c.Received < ScamThreshold {
-					c.Receipt = "<b style='color:red'>Scammed! " + fmt.Sprint(c.Received) + " is below minimum</b>"
-				} else {
-					c.Receipt = "<b>" + fmt.Sprint(c.Received) + " XMR Received! Superchat sent</b>"
-				}
-
-				if c.Received < MediaMin {
-					c.Media = ""
-				}
-				if c.Msg == "" {
-					c.Msg = "⠀"
-				}
-				if c.Received >= ScamThreshold {
-					f, err := os.OpenFile("log/superchats.csv",
-						os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						log.Println(err)
-					}
-					defer func(f *os.File) {
-						err := f.Close()
-						if err != nil {
-							fmt.Println(err)
-						}
-					}(f)
-					csvAppend := fmt.Sprintf(`"%s","%s","%s","%s"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg), fmt.Sprint(c.Received))
-					if r.FormValue("show") != "true" {
-						csvAppend = fmt.Sprintf(`"%s","%s","%s","%s (hidden)"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg), fmt.Sprint(c.Received))
-					}
-					a, err := os.OpenFile("log/alertqueue.csv",
-						os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						log.Println(err)
-					}
-					defer func(a *os.File) {
-						err := a.Close()
-						if err != nil {
-							fmt.Println(err)
-						}
-					}(a)
-					fmt.Println(csvAppend)
-
-					if _, err := f.WriteString(csvAppend + "\n"); err != nil {
-						log.Println(err)
-					}
-
-					if r.FormValue("show") != "true" {
-						csvAppend = fmt.Sprintf(`"%s","%s","%s","???"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg))
-					}
-
-					if _, err := a.WriteString(csvAppend + "\n"); err != nil {
-						log.Println(err)
-					}
-
-					if enableEmail {
-						if r.FormValue("show") != "true" {
-							mail(c.Name, fmt.Sprint(c.Received)+" (hidden)", c.Msg)
-						} else {
-							mail(c.Name, fmt.Sprint(c.Received), c.Msg)
-						}
-					}
-				}
-			} else {
-				c.Received = 0.000
-			}
-			if logged {
-				c.Receipt = "Found old payment"
-				c.Meta = ""
-			}
-		}
-	}
-
-	for _, tx := range resp2.Result.Pool {
-		if tx.PaymentID == c.PayID {
-			var logged = false
-			file, err := os.Open("log/paid.log")
-			if err != nil {
-				log.Fatalf("failed to open ")
-
-			}
-
-			scanner := bufio.NewScanner(file)
-			scanner.Split(bufio.ScanLines)
-			var text []string
-
-			for scanner.Scan() {
-				text = append(text, scanner.Text())
-			}
-
-			err = file.Close()
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			for _, eachLn := range text {
-				if eachLn == tx.PaymentID {
-					logged = true
-				}
-			}
-
-			if !logged {
-
-				f, err := os.OpenFile("log/paid.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-				if err != nil {
-					log.Println(err)
-				}
-				defer func(f *os.File) {
-					err := f.Close()
-					if err != nil {
-						fmt.Println(err)
-					}
-				}(f)
-				if _, err := f.WriteString(tx.PaymentID + "\n"); err != nil {
-					log.Println(err)
-				}
-
-				c.Meta = ""
-				c.Receipt = strconv.FormatInt(tx.Amount, 10) + "Payment received! It is safe to close the tab"
-				c.Received = float64(tx.Amount) / 1000000000000
-				if c.Received < ScamThreshold {
-					c.Receipt = "<b style='color:red'>Scammed! " + fmt.Sprint(c.Received) + " is below minimum</b>"
-				} else {
-					c.Receipt = "<b>" + fmt.Sprint(c.Received) + " XMR Received! Superchat sent</b>"
-				}
-
-				if c.Received < MediaMin {
-					c.Media = "" // remove media if chatter didn't pay the minimum
-				}
-				if c.Msg == "" {
-					c.Msg = "⠀" // unicode blank space because discord doesnt accept empty messages
-				}
-				if c.Received >= ScamThreshold {
-					f, err := os.OpenFile("log/superchats.csv",
-						os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						log.Println(err)
-					}
-					defer func(f *os.File) {
-						err := f.Close()
-						if err != nil {
-							fmt.Println(err)
-						}
-					}(f)
-					csvAppend := fmt.Sprintf(`"%s","%s","%s","%s"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg), fmt.Sprint(c.Received))
-					if r.FormValue("show") != "true" {
-						csvAppend = fmt.Sprintf(`"%s","%s","%s","%s (hidden)"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg), fmt.Sprint(c.Received))
-					}
-					a, err := os.OpenFile("log/alertqueue.csv",
-						os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						log.Println(err)
-					}
-					defer func(a *os.File) {
-						err := a.Close()
-						if err != nil {
-							fmt.Println(err)
-						}
-					}(a)
-					fmt.Println(csvAppend)
-					if _, err := f.WriteString(csvAppend + "\n"); err != nil {
-						log.Println(err)
-					}
-					if r.FormValue("show") != "true" {
-						csvAppend = fmt.Sprintf(`"%s","%s","%s","???"`, c.PayID, html.EscapeString(c.Name), html.EscapeString(c.Msg))
-					}
-					if _, err := a.WriteString(csvAppend + "\n"); err != nil {
-						log.Println(err)
-					}
-					if enableEmail {
-						if r.FormValue("show") != "true" {
-							mail(c.Name, fmt.Sprint(c.Received)+" (hidden)", c.Msg)
-						} else {
-							mail(c.Name, fmt.Sprint(c.Received), c.Msg)
-						}
-					}
-				}
-			} else {
-				c.Received = 0.000
-			}
-			if logged {
-				c.Receipt = "Found old payment"
-				c.Meta = ""
-			}
-		}
-	}
-	err := checkXMRTemplate.Execute(w, c)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
 
 func indexHandler(w http.ResponseWriter, _ *http.Request) {
 	var i indexDisplay
@@ -847,41 +404,6 @@ func indexHandler(w http.ResponseWriter, _ *http.Request) {
 	i.MinAmnt = ScamThreshold
 	i.Checked = checked
 	err := indexTemplate.Execute(w, i)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-func topwidgetHandler(w http.ResponseWriter, r *http.Request) {
-	u, p, ok := r.BasicAuth()
-	if !ok {
-		w.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	if (u == username) && (p == password) {
-		csvFile, err := os.Open("log/superchats.csv")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer func(csvFile *os.File) {
-			err := csvFile.Close()
-			if err != nil {
-				fmt.Println(err)
-			}
-		}(csvFile)
-
-		// TODO: Add an OBS widget displaying top n donors. Don't include amounts set as hidden by donor
-
-		//csvLines, err := csv.NewReader(csvFile).ReadAll()
-		//if err != nil {
-		//	fmt.Println(err)
-		//}
-
-	} else {
-		w.WriteHeader(http.StatusUnauthorized)
-		return // return http 401 unauthorized error
-	}
-	err := topWidgetTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -941,7 +463,7 @@ func alertHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func checkSolanaBalance(address string, amount float64) bool {
+func getSolanaBalance(address string, amount float64) bool {
 
 	balance, err := c.GetBalance(
 		context.TODO(), // request context
@@ -1096,15 +618,4 @@ func paymentHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
-
-	/*
-		// Return result to user
-		if hasBalance { // if wallet has the needed balance
-			//addNewSuperchat()
-		} else {
-			//w.Write([]byte("Solana Address checked: " + wallet_.KeyPublic))
-			w.Write([]byte("Solana Address amount needed:" + donoStr))
-			w.Write([]byte("Address does not have enough balance"))
-		}
-	*/
 }
